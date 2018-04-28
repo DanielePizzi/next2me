9051e369862336f7c49933c2472a5ab7fb2393cd
piacentero <piacentero89@gmail.com>
2018-04-28 10:18:00 +0200

    Update TestInput.java
    Aggiunto campi di input per test e toString

--- Presentation/com.uniroma2.prese.webapp/src/main/java/next2me/model/request/TestInput.java
+++ Presentation/com.uniroma2.prese.webapp/src/main/java/next2me/model/request/TestInput.java
@@ -4,6 +4,28 @@ import next2me.abstracts.AbstractRequest;
 
 public class TestInput extends AbstractRequest {
 	
-	
+	private String a;
 
+	/**
+	 * @return the a
+	 */
+	public String getA() {
+		return a;
+	}
+
+	/**
+	 * @param a the a to set
+	 */
+	public void setA(String a) {
+		this.a = a;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "TestInput [a=" + a + "]";
+	}
+	
 }
