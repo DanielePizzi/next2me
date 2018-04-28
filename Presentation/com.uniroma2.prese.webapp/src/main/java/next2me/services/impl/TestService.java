270804acca44833fc7f462da605101d568d76da3
piacentero <piacentero89@gmail.com>
2018-04-28 10:17:19 +0200

    Update TestService.java
    Aggiunti logger di test service

--- Presentation/com.uniroma2.prese.webapp/src/main/java/next2me/services/impl/TestService.java
+++ Presentation/com.uniroma2.prese.webapp/src/main/java/next2me/services/impl/TestService.java
@@ -1,5 +1,7 @@
 package next2me.services.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import next2me.abstracts.AbstractRequest;
@@ -9,12 +11,21 @@ import next2me.services.IService;
 
 @Service
 public class TestService implements IService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TestService.class);
 
 	@Override
 	public AbstractResponse call(AbstractRequest request, AbstractResponse response) {
+		
+		String methodName = "call";
+		
+		logger.info("Start service [" + methodName + "]. Input: " + request.toString());
+		
 		TestOutput output = new TestOutput();
 		
 		output.setO(".........asdasdasdasdasd");
+
+		logger.info("End service [" + methodName + "]. Output: " + response.toString());
 		
 		return output;
 	}
