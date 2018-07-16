faed08f8d3a062f4be414a0d67002998383690b0
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2018-07-16 18:01:51 +0200

    inserito autocomplete in homePage

--- Presentation/com.uniroma2.prese.angular/src/app/home-page/home-page.component.ts
+++ Presentation/com.uniroma2.prese.angular/src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
+import { FormControl } from '@angular/forms';
 declare var google: any;
 
 @Component({
@@ -11,44 +13,81 @@ export class HomePageComponent implements OnInit {
   lat: number;
   lng: number;
   zoom: number;
+  map: any;
+  public searchControl: FormControl;
 
-  constructor() {
-  }
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) {}
 
   ngOnInit() {
     this.zoom = 10;
+    this.loadAutocomplete();
+
   }
 
   mapReady($event: any) {
-    this.getNearbyPlace($event);
+    this.loadMap($event);
   }
 
-  getNearbyPlace(map: any) {
+  private loadAutocomplete(){
+    this.searchControl = new FormControl();
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.lat = place.geometry.location.lat();
+          this.lng = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+  }
+
+  private loadMap(map: any) {
+    this.map = map;
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
       this.showPosition(position);
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      let placeService = new google.maps.places.PlacesService(map);
-        placeService.nearbySearch({
-          location: latLng,
-          radius: 500,
-          type: ['pub']
-        }, (results, status) => {
-            this.callback(results, status)
-        });
+      // let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      // let placeService = new google.maps.places.PlacesService(map);
+      //   placeService.nearbySearch({
+      //     location: latLng,
+      //     radius: 500,
+      //     type: ['pub']
+      //   }, (results, status) => {
+      //       this.callback(results, status)
+      //   });
       });
     } else {
-      alert("Geolocation is not supported by this browser.");
+      alert("Geolocalizzazione non supportata sul tuo brownser");
     }
   }
 
-  callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        console.log(results)
-      }
-    }
-  }
+  // callback(results, status) {
+  //   if (status === google.maps.places.PlacesServiceStatus.OK) {
+  //     for (var i = 0; i < results.length; i++) {
+  //       console.log(results)
+  //     }
+  //   }
+  // }
 
   private showPosition(position) {
     this.lat = position.coords.latitude;
--- Presentation/com.uniroma2.prese.angular/src/app/home-page/home-page.module.ts
+++ Presentation/com.uniroma2.prese.angular/src/app/home-page/home-page.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomePageComponent } from './home-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AgmCoreModule } from '@agm/core';
@@ -18,7 +19,9 @@ const routes: Routes = [
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCRceEo_Xe9yYkhpjwPzrB8y_L2dCe4by0',
       libraries: ["places"]
-    })
+    }),
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [HomePageComponent],
   exports:[
